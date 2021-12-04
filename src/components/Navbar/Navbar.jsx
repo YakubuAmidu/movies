637ac365762ca931b5b7dba885058a8bf6c185c0
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-04 04:02:22 -0800

    Initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Navbar.scss';
+import logo from '../../img/netflix.png';
+import { Search, Notifications, ArrowDropDown } from '@material-ui/icons';
+import profile from '../../img/profile.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            Navbar
+            <div className="container">
+                <div className="left">
+                  <img src={logo} alt="logo" className="netflix-logo" />
+                  <span className="">Homepage</span>
+                  <span className="">Series</span>
+                  <span className="">Movies</span>
+                  <span className="">New and Popular</span>
+                  <span className="">My List</span>
+                </div>
+                <div className="right">
+                  <Search />
+                  <span>KID</span>
+                  <Notifications />
+                  <img src={profile} className="profile" alt="profile" />
+                  <ArrowDropDown />
+                </div>
+            </div>
         </div>
     )
 }
