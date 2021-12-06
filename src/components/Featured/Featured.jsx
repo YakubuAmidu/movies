25b5845250e4d22259270557c326e1e69d710c4c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-06 13:17:23 -0800

    Initial commit

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Netflix from '../../img/Netflix1.png';
+import InfoImg from '../../img/Featured.png';
+import { InfoOutlined, PlayArrow } from '@material-ui/icons';
 import './Featured.scss';
 
 const Featured = () => {
     return (
         <div className="featured">
-            Featured
+            <img src={Netflix} alt="img" className="featuredImg" />
+            <div className="info">
+                <img src={InfoImg} alt="img" className="featuredInfo" />
+                <span className="desc">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
+                     deserunt. Odio incidunt magnam earum, enim debitis rerum ducimus 
+                     repellendus expedita animi voluptatem, ut nostrum. 
+                     Recusandae nostrum quia possimus odit commodi!
+                </span>
+                <div className="buttons">
+                    <button className="Play">
+                        <PlayArrow />
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlined />
+                        <span>Info</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
