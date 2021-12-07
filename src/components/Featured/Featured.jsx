1968c097e35c03271b8a02ded8e57e8cd037b8dd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-06 23:58:54 -0800

    Initial commit

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -4,9 +4,30 @@ import Matrix from '../../img/matrix.png';
 import { InfoOutlined, PlayArrow } from '@material-ui/icons';
 import './Featured.scss';
 
-const Featured = () => {
+const Featured = ({type}) => {
     return (
         <div className="featured">
+           {type && (
+               <div className="category">
+                   <span>{type === "Movie" ? "Movies" : "Series"}</span>
+                   <select name="genre" id="genre">
+                       <option>Genre</option>
+                       <option value="adventure">Adventure</option>
+                       <option value="comedy">Comedy</option>
+                       <option value="crime">Crime</option>
+                       <option value="fantasy">Fantasy</option>
+                       <option value="historical">Historical</option>
+                       <option value="Horror">Horror</option>
+                       <option value="romantic">Romantic</option>
+                       <option value="sci-fi">Sci-fi</option>
+                       <option value="thriller">Thriller</option>
+                       <option value="western">Western</option>
+                       <option value="animation">Animation</option>
+                       <option value="drama">Drama</option>
+                       <option value="documentary">Documentary</option>
+                   </select>
+               </div>
+           )}
             <img src={Netflix} alt="img" className="featuredImg" />
             <div className="info">
                 <img src={Matrix} alt="img" className="featuredInfo" />
