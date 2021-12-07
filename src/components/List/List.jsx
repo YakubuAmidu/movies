dd2a05d55b48ac2b9e64315d67447c879182f7e3
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-07 01:40:26 -0800

    Initial commit

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './List.scss';
 import ListItem from '../ListItem/ListItem';
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
 
 const List = () => {
+    const [slideNumber, setSlideNumber] = useState(0);
+
+    const listRef = useRef();
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+      if(direction === 'left' && slideNumber > 0){
+          setSlideNumber(slideNumber - 1);
+         listRef.current.style.transform = `translateX(${230 + distance}px)`
+      }
+
+      if(direction === 'right' && slideNumber < 5){
+          setSlideNumber(slideNumber + 1);
+          listRef.current.style.transform = `translateX(${-230 + distance}px)`
+      }
+      console.log(distance);
+    }
+
     return (
         <div className="list">
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-              <ArrowBackIosOutlined className="sliderArrow left"/>
-              <div className="container">
-               <ListItem />
-               <ListItem />
-               <ListItem />
+              <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick("left")}/>
+              <div className="container" ref={listRef}>
                <ListItem />
                <ListItem />
                <ListItem />
@@ -24,7 +39,7 @@ const List = () => {
                <ListItem />
                <ListItem />
               </div>
-              <ArrowForwardIosOutlined className="sliderArrow right"/>
+              <ArrowForwardIosOutlined className="sliderArrow right" onClick={() => handleClick("right")}/>
             </div>
         </div>
     )
