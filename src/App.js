b329ff0537a0db7d60886c79372b34596fe15f67
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-08 22:38:11 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.scss';
 //import Home from './pages/Home/Home';
 //import Watch from './pages/Watch/Watch';
-import Register from './pages/register/Register';
+import Login from './pages/Login/Login';
+//import Register from './pages/register/Register';
 
 const App = () => {
   return (
     <div className="App">
-     <Register />
+     <Login />
     </div>
   );
 }
