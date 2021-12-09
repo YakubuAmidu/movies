d0dcea0d4829f2a612b968bb5771537896fc7096
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-08 21:57:56 -0800

    Initial commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import NetflixText from '../../img/Netflixtext.png';
 import './Register.scss';
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value);
+    }
+
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value)
+    }
+
     return (
         <div className="register">
             <div className="top">
@@ -11,17 +25,26 @@ const Register = () => {
                 <button className="loginButton">Sign In</button>
             </div>
             </div>
-            
+
             <div className="container">
              <h1>Unlimited movies, Tv shows and more</h1>
              <h2>Watch anywhere, Cancel anytime</h2>
              <p>
                 Ready to watch ? Enter your email to create or restart your membership.
              </p>
-             <div className="input">
-                 <input type="email" placeholder="Email address" />
-                 <button className="registerButton">Get started</button>
-             </div>
+             {
+                 !email ? (
+                    <div className="input">
+                    <input type="email" placeholder="Email address" ref={emailRef}/>
+                    <button className="registerButton" onClick={handleStart}>Get started</button>
+                </div>
+                 ) : (
+                    <form className="input">
+                    <input type="password" placeholder="Password" ref={passwordRef}/>
+                    <button className="registerButton" onClick={handleFinish}>Started</button>
+                </form>
+                 )
+             }
             </div>
         </div>
     )
