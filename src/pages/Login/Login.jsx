aaf98d1397efd175b874c042848905e4f040998d
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-12-08 22:19:48 -0800

    Initial commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import NetflixTex from '../../img/Netflixtext.png';
 import './Login.scss';
 
 const Login = () => {
     return (
         <div className="login">
-            Login
+            <div className="top">
+                <div className="wrapper">
+                    <img className="logo" src={NetflixTex} alt="img" />
+                </div>
+            </div>
+
+            <div className="container">
+                
+            </div>
         </div>
     )
 }
